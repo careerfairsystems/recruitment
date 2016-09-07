72c0c6c4380adf98c76b747292484b6fe06a9378
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-09-08 00:19:14 +0200

    show latest taskgroup in front-page

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,18 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'TaskgroupsService', '$sce',
+  function ($scope, Authentication, TaskgroupsService, $sce) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    var vm = this;
+    $scope.vm = vm;
+
+    vm.taskapplications = TaskgroupsService.query({ active: true }, function(u, responseHeaders) {
+      vm.activeTaskgroup = u[0];
+      vm.description = $sce.trustAsHtml(vm.activeTaskgroup.description);
+    });
+
+
   }
 ]);
