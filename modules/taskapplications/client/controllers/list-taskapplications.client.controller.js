1afea04baf77b005073dcc267c85d8e5730e1ad5
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-09-15 00:33:38 +0200

    make tables pretty and magical

--- modules/taskapplications/client/controllers/list-taskapplications.client.controller.js
+++ modules/taskapplications/client/controllers/list-taskapplications.client.controller.js
@@ -6,49 +6,75 @@
     .module('taskapplications')
     .controller('TaskapplicationsListController', TaskapplicationsListController);
 
-  TaskapplicationsListController.$inject = ['$scope' ,'TaskapplicationsService'];
+  TaskapplicationsListController.$inject = ['$scope' ,'TaskapplicationsService', '$filter'];
 
-  function TaskapplicationsListController($scope, TaskapplicationsService) {
+  function TaskapplicationsListController($scope, TaskapplicationsService, $filter) {
     var vm = this;
 
     TaskapplicationsService.query(function(data) {
       vm.taskapplications = data;
-      console.log(data[0]);
-      angular.forEach(vm.taskapplications, function(application) {
+      angular.forEach(vm.taskapplications, function(application, key) {
+        application.nr = 1 + key;
+        application.date = $filter('date')(application.created, "yyyy-MM-dd");
         application.choice1 = application.choices[0].choice || '';
         application.choice2 = application.choices[1].choice || '';
         application.choice3 = application.choices[2].choice || '';
       });
       // Datatable code
-      $('#applicationsList').DataTable({
+      // Setup - add a text input to each footer cell
+      $('#applicationsList tfoot th:not(:first)').each(function () {
+        var title = $(this).text();
+        $(this).html('<input class="form-control" type="text" placeholder="Search '+title+'" />');
+      });  
+
+      var table = $('#applicationsList').DataTable({
         dom: 'Bfrtip',
-        'scrollY': 300,
-        'scrollX': true,
+        scrollX: true,
+        scrollCollapse: true,
+        autoWidth: false,
+        paging: false,
         stateSave: true,
         buttons: [
           'copy', 'excel', 'pdf', 'colvis'
         ],
         data: data,
+        'order': [[ 1, 'asc' ]],
         columns: [
-          { data: 'name', title: 'Name' },
-          { data: 'program', title: 'Program' },
-          { data: 'year', title: 'Year' },
-          { data: 'point', title: 'Points' },
-          { data: 'choice1', title: 'First choice' },
-          { data: 'choice2', title: 'Second choice' },
-          { data: 'choice3', title: 'Third choice' },
-          { data: 'attendGasque', title: 'Attend Gasque' },
-          { data: 'attendKickoff', title: 'Attend Kickoff' },
-          { data: 'driverLicense', title: 'DriverLicense' },
-          { data: 'email', title: 'Email' },
-          { data: 'phone', title: 'Phone' },
-          { data: 'tshirtsize', title: 'Tshirtsize' },
-          { data: 'assignedTask', title: 'Assigned Task' }
+          { data: 'nr' },
+          { data: 'date' },
+          { data: 'name',
+            'fnCreatedCell': function (nTd, sData, oData, iRow, iCol) {
+              $(nTd).html('<a href="/taskapplications/'+oData._id+'/'+oData.taskgroup+'">'+sData+'</a>');
+            }
+          },
+          { data: 'program' },
+          { data: 'year' },
+          { data: 'point' },
+          { data: 'choice1' },
+          { data: 'choice2' },
+          { data: 'choice3' },
+          { data: 'attendGasque' },
+          { data: 'attendKickoff' },
+          { data: 'driverLicense' },
+          { data: 'email' },
+          { data: 'phone' },
+          { data: 'tshirtsize' },
+          { data: 'assignedTask' }
         ]
       });
+            
+      // Apply the search
+      table.columns().every(function () {
+        var that = this;
+        $('input', this.footer()).on('keyup change', function () {
+          if (that.search() !== this.value) {
+            that.search(this.value).draw();
+          }
+        });
+      });
+
     });
 
-    
     // For exporting to excel
     $scope.datafields = {
       'name': 'Name',
